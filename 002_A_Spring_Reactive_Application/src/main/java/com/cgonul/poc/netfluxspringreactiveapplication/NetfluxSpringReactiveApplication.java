1b3fd1b0f2f9f64606b857aff9514fd29a42033e
cgonul <caglar.gonul.001@gmail.com>
2018-03-04 15:34:40 +0300

    master Net-flux example is initially constructed.

---
+++ 002_A_Spring_Reactive_Application/src/main/java/com/cgonul/poc/netfluxspringreactiveapplication/NetfluxSpringReactiveApplication.java
@@ -0,0 +1,12 @@
+package com.cgonul.poc.netfluxspringreactiveapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetfluxSpringReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetfluxSpringReactiveApplication.class, args);
+	}
+}
