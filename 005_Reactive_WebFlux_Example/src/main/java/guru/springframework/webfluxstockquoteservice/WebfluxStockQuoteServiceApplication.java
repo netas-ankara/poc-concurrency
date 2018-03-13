11bac4b227127b7c30965c23a39e92019f46e7be
Caglar Gonul <cgonul@netas.com.tr>
2018-03-13 17:51:24 +0300

    master Working example is implemented.

--- 005_Reactive_WebFlux_Example/src/main/java/guru/springframework/webfluxstockquoteservice/WebfluxStockQuoteServiceApplication.java
+++ 005_Reactive_WebFlux_Example/src/main/java/guru/springframework/webfluxstockquoteservice/WebfluxStockQuoteServiceApplication.java
@@ -1,12 +1,12 @@
-package com.poc.webfluxdemo;
+package guru.springframework.webfluxstockquoteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebfluxDemoApplication {
+public class WebfluxStockQuoteServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebfluxDemoApplication.class, args);
+		SpringApplication.run(WebfluxStockQuoteServiceApplication.class, args);
 	}
 }
